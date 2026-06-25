e0366a1a58bda91305dd18b0674961a8f7300640
freshjena-bit <freshjena@gmail.com>
2026-06-25 16:30:53 +0700

    Create applications.js

---
+++ data/applications.js
@@ -0,0 +1,10 @@
+export const applications = [
+  {
+    id: "react-dashboard",
+    title: "React Dashboard",
+    description: "Admin dashboard React",
+    size: "8 MB",
+    category: "Application",
+    file: "/applications/dashboard.zip"
+  }
+];
