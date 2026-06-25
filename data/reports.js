f462bcccb8b7585bd710ecc65635b3abf759c0e7
freshjena-bit <freshjena@gmail.com>
2026-06-25 16:31:30 +0700

    Create reports.js

---
+++ data/reports.js
@@ -0,0 +1,10 @@
+export const reports = [
+  {
+    id: "annual-report",
+    title: "Annual Report",
+    description: "Laporan tahunan",
+    size: "3 MB",
+    category: "Report",
+    file: "/reports/report.pdf"
+  }
+];
