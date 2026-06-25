260070ef9ec88d9291c1ef8a9e7c36936ba9685c
freshjena-bit <freshjena@gmail.com>
2026-06-25 16:42:40 +0700

    Create Footer.jsx

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="border-t border-slate-800 mt-10">
+
+      <div className="p-6 text-center text-slate-400">
+
+        Public Archive Pro © 2026
+
+      </div>
+
+    </footer>
+  );
+}
