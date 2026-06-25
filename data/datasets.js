32fd3d4399e476ceb04e746f5a8add15a0be4d94
freshjena-bit <freshjena@gmail.com>
2026-06-25 16:31:11 +0700

    Create datasets.js

---
+++ data/datasets.js
@@ -0,0 +1,10 @@
+export const datasets = [
+  {
+    id: "sample-dataset",
+    title: "Public Dataset",
+    description: "Dataset publik contoh",
+    size: "15 MB",
+    category: "Dataset",
+    file: "/datasets/data.zip"
+  }
+];
